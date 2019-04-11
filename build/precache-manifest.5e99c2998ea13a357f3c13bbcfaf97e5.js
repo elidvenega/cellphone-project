6442c5e54b163652e8ec2c9e2ba63cf587fc8041
EVUX <venegaelid@gmail.com>
2019-04-10 20:38:44 -0700

    Add files via upload

---
+++ build/precache-manifest.5e99c2998ea13a357f3c13bbcfaf97e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "27eada894fc6545b3c58",
+    "url": "/static/css/main.db23ed81.chunk.css"
+  },
+  {
+    "revision": "27eada894fc6545b3c58",
+    "url": "/static/js/main.feddcb4a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4ad94dd1404889968517",
+    "url": "/static/css/2.8848549e.chunk.css"
+  },
+  {
+    "revision": "4ad94dd1404889968517",
+    "url": "/static/js/2.8b406049.chunk.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/static/media/logo.9f7e506d.svg"
+  },
+  {
+    "revision": "c7d0114d36f21d496e15536f31ef0472",
+    "url": "/index.html"
+  }
+];
