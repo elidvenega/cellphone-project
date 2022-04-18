5b00cebd775f7b9e291a4b304b88f8a3b917f8d1
elidvenega <venegaelid@gmail.com>
2022-04-17 22:58:57 -0700

    packages were updated

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {BrowserRouter as Router} from 'react-router-dom';
-import App from './App';
-import {ProductProvider} from './context';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App";
+import { ProductProvider } from "./context";
 
-ReactDOM.render(
-<ProductProvider> 
-<Router>    
-<App />
-</Router>
-</ProductProvider>   
-,
-document.getElementById('root'));
+// ReactDOM.render(
+//   <ProductProvider>
+//     <Router>
+//       <App />
+//     </Router>
+//   </ProductProvider>,
+//   document.getElementById("root")
+// );
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <ProductProvider>
+    <Router>
+      <App />
+    </Router>
+  </ProductProvider>
+);
